16d8c5bb57c1375b2fb5821b2fd566682908fb1d
Alex Visan <visanpaypal@gmail.com>
2021-09-05 02:11:02 +0200

    2nd commit

--- index.js
+++ index.js
@@ -45,12 +45,11 @@ function show(data) {
 }
 
 function exportReportToExcel() {
-  let table = document.getElementsByTagName("table"); // you can use document.getElementById('tableId') as well by providing id to the table tag
-  TableToExcel.convert(table[0], {
-    // html code may contain multiple tables so here we are refering to 1st table tag
-    name: `export.xlsx`, // fileName you could use any name
-    sheet: {
-      name: "Sheet 1", // sheetName
-    },
-  });
-}
+    let table = document.getElementsByTagName("table"); // you can use document.getElementById('tableId') as well by providing id to the table tag
+    TableToExcel.convert(table[0], { // html code may contain multiple tables so here we are refering to 1st table tag
+      name: `export.xlsx`, // fileName you could use any name
+      sheet: {
+        name: 'Sheet 1' // sheetName
+      }
+    });
+  }
